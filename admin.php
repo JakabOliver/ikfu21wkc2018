a2731cce5c3b28e4539a69c3ff65bfd343f0a8ee
Olivér <olika96@gmail.com>
2018-07-04 14:50:05 +0200

    Backend

---
+++ admin.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oliver
+ * Date: 2018-07-03
+ * Time: 22:41
+ */
+session_start();
+
+if(!empty($_POST)){
+    if(!empty($_POST['login'])) {
+        if ($_POST['username'] == 'media' && $_POST['password'] == 'Almafa') {
+            $_SESSION['loggedin'] = true;
+            echo 'sikeres login';
+        } else {
+            echo 'sikertelen belépés';
+        }
+    }elseif(isset($_POST['upload'])){
+        if(empty(trim($_POST['title'])))
+            echo 'A cím nem lehet üres<br/>';
+        elseif(empty(trim($_POST['content'])))
+            echo 'A tartalom nem lehet üres<br/>';
+        else{
+            file_put_contents('news/'.ekezetmentesites(trim($_POST['title'])).'.json', json_encode($_POST));
+            echo 'sikeres feltöltés';
+        }
+    }
+}
+
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']){
+    $html=file_get_contents("parts/upload.html");
+    $newsListTemplate=file_get_contents("parts/newsListTemplate.html");
+    $newsList='';
+    $scanned_directory = array_diff(scandir('news/'), array('..', '.'));
+    foreach($scanned_directory as $news){
+        $newsArray=json_decode(file_get_contents('news/'.$news));
+        $newsList.=str_replace('$title', $newsArray->title, $newsListTemplate);
+    }
+    $html=str_replace('<news>', $newsList, $html);
+}else{
+    $html=file_get_contents("parts/login.html");
+}
+echo $html;
+
+
+function ekezetmentesites($var){
+     $search=array('á', 'é', 'í', 'ó', 'ö', 'ő', 'ú', 'ü', 'ű', 'Á', 'É', 'Í', 'Ó', 'Ö', 'Ő', 'Ú', 'Ü', 'Ű', ' ');
+    $replace=array('a', 'e', 'i', 'o', 'o', 'o', 'u', 'u', 'u', 'A', 'E', 'I', 'O', 'O', 'O', 'U', 'U', 'U', '_');
+    return str_replace($search, $replace, $var);
+}
--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
 
+$teams=['Taiwan','Belgium', 'China', 'Czech Republic', 'England', 'Germany', 'Hong Kong', 'Hungary', 'Netherlands', 'Portugal', 'Turkey'];
+
 $mod="main";
 if(isset($_GET['mod'])){
     $mod=$_GET['mod'];
 }
+
 
 $html="";
 $head=file_get_contents("parts/head.html");
 $content=file_get_contents("parts/".$mod.".html");
 $footer=file_get_contents("parts/footer.html");
 
+if($mod=='teams'){
+    $teamTemplate=file_get_contents('parts/teamTemplate.html');
+    $teamsContent='';
+    foreach($teams as $k=>$team){
+        $teampic=str_replace(' ', '_', $team);
+        $teamsContent.=str_replace(array('$teampic', '$team'), array($teampic, $team), $teamTemplate);
+    }
+    //generate the content.
+    $content=str_replace('<teams>', $teamsContent, $content);
+}elseif($mod=='main'){
+    $newsTemplate=file_get_contents('parts/newsTemplate.html');
+    $newsContent='';
+    $scanned_directory = array_diff(scandir('news/'), array('..', '.'));
+    foreach($scanned_directory as $news){
+        $newsArray=json_decode(file_get_contents('news/'.$news));
+        $newsContent.=str_replace(array('$title', '$content'), array($newsArray->title, $newsArray->content), $newsTemplate);
+    }
+    //beolvasás
+    $content=str_replace('<news>', $newsContent, $content);
+}
 
 
 $html=$head.$content.$footer;
